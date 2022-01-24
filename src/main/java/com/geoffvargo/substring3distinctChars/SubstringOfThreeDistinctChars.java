2f23c0c0c2c6f5b796cb2af13d37dd05de7382a0
geoffvargo <geoff.vargo@stonybrook.edu>
2022-01-24 02:44:00 -0500

    INITIAL COMMIT

---
+++ src/main/java/com/geoffvargo/substring3distinctChars/SubstringOfThreeDistinctChars.java
@@ -0,0 +1,30 @@
+package com.geoffvargo.substring3distinctChars;
+
+class SubstringOfThreeDistinctChars {
+	/**
+	 * A string is <b>good <i>if there are no repeated characters</i></b>.
+	 * Given a string <b><code>s</code></b>, return the number of <b>good substrings of length three</b> in <b><code>s</code></b>.
+	 * Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+	 * <p/>
+	 * A <b><i>substring</i></b> is a contiguous sequence of characters in a string.
+	 * <p/>
+	 * <h3>Example 1:</h3>
+	 *
+	 * <blockquote>
+	 *<b>Input:</b> <pre>s = "xyzzaz"</pre><br>
+	 * <b>Output:</b> 1<br>
+	 * <b>Explanation:</b> There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz".
+	 * The only good substring of length 3 is "xyz". </blockquote>
+	 * <p/>
+	 * <h3>Example 2:</h3>
+	 * <blockquote>
+	 * <b>Input:</b> s = "aababcabc"<br>
+	 * <b>Output:</b> 4<br>
+	 * <b>Explanation:</b> There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc".
+	 * The good substrings are "abc", "bca", "cab", and "abc".
+	 * </blockquote>
+	 */
+	public int countGoodSubstrings(String s) {
+		return 0;
+	}
+}
